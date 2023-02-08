1b0fa86937e6b770ddfadb9be4a31d00270594d3
phongLH18715 <trunght.yrc@gmail.com>
2023-02-08 16:30:45 +0700

    added Bi-LSTM model

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,8 +1,15 @@
+const tf = require('@tensorflow/tfjs');
+
 const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
     console.log('Hello');
 });
+
+async function classify() {
+    const model = await tf.loadLayersModel('./classifer/model.json');
+    model.predict();
+}
 
 module.exports = router;
