ddecfb0cabafa78d7cc9f51b2e13c516a3e1948f
phongLH18715 <trunght.yrc@gmail.com>
2023-02-08 17:45:06 +0700

    fixed relocation

--- index.js
+++ index.js
@@ -3,21 +3,15 @@ const express = require("express");
 const app = express();
 const reportController = require("./controllers/reportController");
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.json());
 
 app.use("/report", reportController);
 
 app.get('/', (req, res) => {
-    console.log('Hello')
+    res.send('Hello world');
 })
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
-
+exports.app = functions.https.onRequest(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
