7091bf460bf290cfb953a4be1d2f495cc6803155
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2023-02-13 10:12:12 +0700

    accel data file recieve using multer

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,13 +1,36 @@
-
 const express = require("express");
 const ClassifyService = require("../services/classifyService");
-const Pedometer = require('../services/pedometerService');
+const Pedometer = require("../services/pedometerService");
 const router = express.Router();
 
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + "/../dataset/");
+    },
+    filename: function (req, file, cb) {
+        var today = new Date();
+        cb(null, file.fieldname + "-" + today.getTime() + ".txt");
+    },
+});
+
+const upload = multer({ storage: storage });
+
 router.get("/", async (req, res) => {
-    console.log('Hello');
-    const classifiedData = Pedometer.loadClassified('./classifiedData/classified.txt')
-    Pedometer.stepCounting(classifiedData)
+    console.log("Hello");
+    const classifiedData = Pedometer.loadClassified(
+        "./classifiedData/classified.txt"
+    );
+    Pedometer.stepCounting(classifiedData);
+});
+
+router.post("/upload", upload.single("accel"), async (req, res) => {
+    if (req.file) {
+        res.send("File saved at: " + req.file.path);
+    } else {
+        res.send("No files attached");
+    }
 });
 
 module.exports = router;
